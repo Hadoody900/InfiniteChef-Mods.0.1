54c5af9dded26de2dcfd67ccdff208aed6471c6b
Hadoody900 <haiadiraliami@gmail.com>
2024-05-15 12:20:52 +0300

    Create nutters.js

---
+++ Nutters.js
@@ -0,0 +1,12 @@
+//this is my first mod ;)//
+addIngredient("nutters", {
+    color:"#c93a16",
+    type:"plant",
+    shape:"cusp_circle_up",
+    group:"protein_plant",
+    hidden:false,
+    dishWeight:-20,
+    scale:0.75,
+    broken:"ground_nut"
+}
+);
